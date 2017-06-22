194ecfffb5dc30711a18463fb7e935476b0eb0d2
tedi3231 <tedi3231@gmail.com>
2017-06-22 10:32:31 +0800

    use redis pool connection

--- addons/redis/__init__.py
+++ addons/redis/__init__.py
@@ -4,13 +4,12 @@ from odoo.tools import config as openerp_config
 from odoo import http
 from odoo.http import Root as OdooRoot
 from odoo.http import OpenERPSession
-from odoo.http import session_gc
 from werkzeug.contrib.sessions import SessionStore
 import cPickle
 import logging
 import redis
 
-ONE_WEEK_IN_SECONDS = 60*60*24*7
+ONE_WEEK_IN_SECONDS = 60 * 60 * 24 * 7
 
 logger = logging.getLogger(__name__)
 
@@ -36,54 +35,51 @@ class RedisSessionStore(SessionStore):
         try:
             return self.redis.setex(key, ONE_WEEK_IN_SECONDS, data)
         except Exception as ex:
-            logger.error("Error on setting session data", exc_info = ex)
+            logger.error("Error on setting session data", exc_info=ex)
 
     def delete(self, session):
         try:
             key = self.get_session_key(session.sid)
             return self.redis.delete(key)
         except Exception as ex:
-            logger.error("Error on deleting session data", exc_info = ex)
+            logger.error("Error on deleting session data", exc_info=ex)
 
     def get(self, sid):
         if not self.is_valid_key(sid):
             return self.new()
-        
+
         key = self.get_session_key(sid)
         try:
             saved = self.redis.get(key)
             data = cPickle.loads(saved) if saved else {}
         except Exception as ex:
-            logger.error("Error on getting session data", exc_info = ex)
+            logger.error("Error on getting session data", exc_info=ex)
             data = {}
         return self.session_class(data, sid, False)
 
 
-use_redis   = openerp_config.get('use_redis', False)
+use_redis = openerp_config.get('use_redis', False)
 
 logger.debug("Enable Redis : {}".format(use_redis))
 
 if use_redis:
-    redis_host  = openerp_config.get('redis_host', 'localhost')
-    redis_port  = openerp_config.get('redis_port', 6379)
-    redis_db = openerp_config.get('redis_db',0)
-    redis_salt  = openerp_config.get(
-                    'redis_salt',
-                    '-RMsSz~]3}4[Bu3_aEFx.5[57O^vH?`{X4R)Y3<Grvq6E:L?6#aoA@|/^^ky@%TI'
-                    )
+    redis_host = openerp_config.get('redis_host', 'localhost')
+    redis_port = openerp_config.get('redis_port', 6379)
+    redis_db = openerp_config.get('redis_db', 0)
+    redis_salt = openerp_config.get('redis_salt',
+                                    '-RMsSz~]3}4[Bu3_aEFx.5[57O^vH?`{X4R)Y3<Grvq6E:L?6#aoA@|/^^ky@%TI')
     logger.debug("Connecting Redis at {}:{}".format(redis_host, redis_port))
     # TODO: connection pool option
-    redis_instance = redis.StrictRedis( host=redis_host, port=redis_port, db=redis_db )
-
+    redis_pool = redis.ConnectionPool(host=redis_host, port=redis_port, db=redis_db)
+    redis_instance = redis.StrictRedis(connection_pool=redis_pool)
 
     class Root(OdooRoot):
         @lazy_property
         def session_store(self):
             return RedisSessionStore(
-                    redis_instance,
-                    redis_salt,
-                    session_class=OpenERPSession)
-
+                redis_instance,
+                redis_salt,
+                session_class=OpenERPSession)
 
     http.root = Root()
 
